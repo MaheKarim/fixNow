31f49a66c7bb4631036b4817ccf7a14024beb69a
Mahe Karim <mahekarim@deligram.com>
2022-01-15 19:17:43 +0600

    Admin Panel Change :zap:

--- resources/views/layouts/backend/partials/footer.blade.php
+++ resources/views/layouts/backend/partials/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="app-footer">
         <div class="app-footer__inner">
             <div class="app-footer-left">
-                Developed by 💖 <a href="https://mahekarim.me" target="_blank">InnoX</a>
+                Developed by 💖 <a href="https://#.me" target="_blank">FixNow</a>
             </div>
         </div>
     </div>
