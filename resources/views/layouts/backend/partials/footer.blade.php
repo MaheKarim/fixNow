ae25da26a3928a1f570ca34d780d8dec3ef23397
Mahe Karim <mahekarim@deligram.com>
2022-01-15 19:19:54 +0600

    Admin Panel Change :zap:

--- resources/views/layouts/backend/partials/footer.blade.php
+++ resources/views/layouts/backend/partials/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="app-footer">
         <div class="app-footer__inner">
             <div class="app-footer-left">
-                Developed by 💖 <a href="https://#.me" target="_blank">FixNow</a>
+{{--                Developed by 💖 <a href="https://#.me" target="_blank">{{ config('app.name') }}</a>--}}
             </div>
         </div>
     </div>
